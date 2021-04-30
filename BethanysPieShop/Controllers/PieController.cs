f36f25878909dcd77775e23f2b61fc4aaa954d04
redmondthomas <78367834+redmondthomas@users.noreply.github.com>
2021-04-30 18:13:20 +0100

    Update PieController.cs

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -6,8 +6,6 @@ using BethanysPieShop.Models;
 using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace BethanysPieShop.Controllers
 {
     public class PieController : Controller
